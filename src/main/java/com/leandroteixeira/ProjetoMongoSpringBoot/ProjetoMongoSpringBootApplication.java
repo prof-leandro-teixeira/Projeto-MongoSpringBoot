9d65111ad67e9dacd57a0224e6b61ea1040904b1
prof-leandro-teixeira <leandro_prof@yahoo.com>
2022-10-04 19:19:59 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/leandroteixeira/ProjetoMongoSpringBoot/ProjetoMongoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.leandroteixeira.ProjetoMongoSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoMongoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoMongoSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leandroteixeira/ProjetoMongoSpringBoot/ProjetoMongoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leandroteixeira.ProjetoMongoSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoMongoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
